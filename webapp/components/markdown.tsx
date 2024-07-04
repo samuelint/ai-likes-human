aa3daa7cf96d7adf2efe925b41cd4c2db0f3ee23
Samuel <samuelint@gmail.com>
2024-07-04 09:45:08 -0400

    fix: build

--- webapp/components/markdown.tsx
+++ webapp/components/markdown.tsx
@@ -1,6 +1,7 @@
+'use client';
 import LibMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { Prism } from 'react-syntax-highlighter';
 import { oneDark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 
 
@@ -46,7 +47,7 @@ export function Markdown({ children }: Props) {
 
           return match ? (
           // @ts-expect-error - Error from library
-            <SyntaxHighlighter
+            <Prism
               {...rest}
               PreTag="div"
               // eslint-disable-next-line react/no-children-prop

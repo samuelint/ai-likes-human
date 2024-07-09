2c37520d9987420ee00c45c3cc6914d164e710cf
Samuel <samuelint@gmail.com>
2024-07-09 08:56:40 -0400

    fix: display run info in tooltip on assistant avatar

--- webapp/components/chat-ai-message.tsx
+++ webapp/components/chat-ai-message.tsx
@@ -1,6 +1,8 @@
 import { Run } from 'openai/resources/beta/threads/runs/runs.mjs';
 import { ChatMessageProps, Message } from './message';
-import { MessageRunDetails } from './message-run-details';
+import { Avatar, AvatarFallback } from './ui/avatar';
+import { MessageRunDetailsTooltip } from './message-run-details';
+
 
 
 interface Props extends Omit<ChatMessageProps, 'type' | 'className' | 'actions'> {
@@ -8,12 +10,20 @@ interface Props extends Omit<ChatMessageProps, 'type' | 'className' | 'actions'>
 }
 
 export function AIChatMessage({ run, children, ...props }: Props) {
-  return <Message
-    type='ai'
-    className='w-max max-w-[75%]'
-    {...props}
-  >
-    {run && <MessageRunDetails run={run} />}
-    {children}
-  </Message>;
+  return (
+    <div className='flex'>
+      <MessageRunDetailsTooltip run={run}>
+        <Avatar className='select-none'>
+          <AvatarFallback>A</AvatarFallback>
+        </Avatar>
+      </MessageRunDetailsTooltip>
+      <Message
+        type='ai'
+        className='w-max max-w-[75%]'
+        {...props}
+      >
+        {children}
+      </Message>
+    </div>
+  );
 }
--- webapp/components/message-run-details.tsx
+++ webapp/components/message-run-details.tsx
@@ -1,21 +1,21 @@
 import { TooltipProvider } from '@radix-ui/react-tooltip';
 import { Run } from 'openai/resources/beta/threads/runs/runs.mjs';
 import { Tooltip, TooltipContent, TooltipTrigger } from './ui/tooltip';
-import { Button } from './ui/button';
-import { Info } from 'lucide-react';
 import { toFromNowFormattedDate } from '@/lib/date';
+import { ReactNode } from 'react';
 
 
 interface Props {
-  run: Pick<Run, 'model' | 'created_at'>
+  run?: Pick<Run, 'model' | 'created_at'> | null
+  children?: ReactNode
 }
 
-export function MessageRunDetails({ run }: Props) {
-  return (
-    <TooltipProvider delayDuration={100}>
+export function MessageRunDetailsTooltip({ run, children }: Props) {
+  return run && (
+    <TooltipProvider delayDuration={400}>
       <Tooltip>
         <TooltipTrigger asChild>
-          <Button variant="ghost" size="icon" className='w-6 h-6 text-slate-200 hover:text-slate-500'><Info className='w-4 h-4'/></Button>
+          { children }
         </TooltipTrigger>
         <TooltipContent side='bottom'>
           <div className='flex flex-col font-light text-slate-600 text-xs select-none'>

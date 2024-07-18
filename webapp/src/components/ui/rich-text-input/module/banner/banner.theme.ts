233139880c44c424f10f53299e35308ccb280eb4
Samuel Richard Magny <samuelint@gmail.com>
2024-07-18 12:13:35 -0400

    fix: bug with code block (#37)

--- webapp/src/components/ui/rich-text-input/module/banner/banner.theme.ts
+++
@@ -1,9 +0,0 @@
-export interface BannerTheme {
-  banner: string
-}
-
-const banner: BannerTheme['banner'] = 'border-l-4 border-indigo-500 px-2';
-
-const bannerTheme: BannerTheme = { banner };
-
-export { bannerTheme };

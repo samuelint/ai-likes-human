4fb21a3697377b52f03a47bbafed6716293a47fa
Samuel Richard Magny <samuelint@gmail.com>
2024-07-22 13:42:35 -0400

    feat: use ui-ball loading (#45)

--- webapp/src/components/splashscreen.tsx
+++ webapp/src/components/splashscreen.tsx
@@ -1,10 +1,13 @@
-import { ThreeDotsLoading } from '@/components/ui/loading';
+import { HelixLoading } from '@/components/ui/loading';
 
 export default function Splashscreen() {
   return (
-    <div className="flex flex-col items-center justify-center gap-4 fixed inset-0 backdrop-filter backdrop-blur-sm z-50">
-      <p>Loading</p>
-      <ThreeDotsLoading />
+    <div className="flex flex-col items-center justify-center gap-6 fixed inset-0 backdrop-filter backdrop-blur-sm z-50">
+      <div className='flex flex-col'>
+        <h1 className='text-3xl font-medium tracking-tighter'>Launching Assistant</h1>
+        <p>Please wait</p>
+      </div>
+      <HelixLoading />
     </div>
   );
 

09b2610ae8cf571d442e3083e2b00cfaacb9fede
Samuel Richard Magny <samuelint@gmail.com>
2024-07-23 12:42:05 -0400

    fix: local inference (#46)

--- core/ai_assistant_core/llm/infrastructure/llamacpp_llm_factory.py
+++ core/ai_assistant_core/llm/infrastructure/llamacpp_llm_factory.py
@@ -1,15 +1,44 @@
-from typing import Optional
+from typing import List, Optional
 
 from injector import inject
 from langchain_core.language_models import BaseChatModel
+from langchain_core.messages import BaseMessage
 
-from langchain_llamacpp_chat_model import LlamaProxyChatModel
+from langchain_llamacpp_chat_model.proxy import LlamaProxyChatModel
 
 from ai_assistant_core.llm.infrastructure.llamacpp_proxy_factory import (
     LlamaCppProxyFactory,
 )
+from langchain_openai_api_bridge.chat_model_adapter.llamacpp.llamacpp_openai_compatible_chat_model_adapter import (
+    LlamacppOpenAICompatibleChatModelAdapter,
+)
 
 from ..domain.base_llm_factory import BaseLLMFactory
+
+
+class LLamacppProxyOpenAICompatibleChatModel(LlamaProxyChatModel):
+
+    adapter = LlamacppOpenAICompatibleChatModelAdapter()
+
+    def _stream(self, messages: List[List[BaseMessage]], **kwargs):
+        transformed_messages = self.adapter.to_openai_format_messages(messages)
+
+        return super()._stream(messages=transformed_messages, **kwargs)
+
+    def _astream(self, messages: List[List[BaseMessage]], **kwargs):
+        transformed_messages = self.adapter.to_openai_format_messages(messages)
+
+        return super()._astream(transformed_messages, **kwargs)
+
+    def _generate(self, messages: List[List[BaseMessage]], **kwargs):
+        transformed_messages = self.adapter.to_openai_format_messages(messages)
+
+        return super().generate(transformed_messages, **kwargs)
+
+    def _agenerate(self, messages: List[List[BaseMessage]], **kwargs):
+        transformed_messages = self.adapter.to_openai_format_messages(messages)
+
+        return super()._agenerate(transformed_messages, **kwargs)
 
 
 @inject
@@ -33,7 +62,7 @@ class LlamaCPPFactory(BaseLLMFactory):
         temperature: Optional[float] = 0,
     ) -> BaseChatModel:
         llama_proxy = self.factory.get_llama_proxy()
-        return LlamaProxyChatModel(
+        return LLamacppProxyOpenAICompatibleChatModel(
             llama_proxy=llama_proxy,
             model_name=model,
             max_tokens=max_tokens,

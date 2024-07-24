014f65b51ccc31b594f8222e37191828cbfc099c
Samuel <samuelint@gmail.com>
2024-07-24 09:06:46 -0400

    chore: split desktop app dir

--- desktop/copy-server-bin.sh
+++ desktop/copy-server-bin.sh
@@ -3,14 +3,14 @@
 script_dir=$(dirname "$(readlink -f "$0")")
 
 # Define the relative paths to the binaries
-tauri_dist_binary="$script_dir/src-tauri/bin"
+tauri_dist_binary="$script_dir/bin"
 core_dist_binary="$script_dir/../core/dist"
 
 # Create the tauri_dist_binary directory if it does not exist
 mkdir -p "$tauri_dist_binary"
 
 # Copy all files in core_dist_binary to tauri_dist_binary
-cp -r "$core_dist_binary/"* "$tauri_dist_binary"
+cp -rf "$core_dist_binary/"* "$tauri_dist_binary"
 
 # make all files in tauri_dist_binary executable
 if [[ "$OSTYPE" != "msys" ]]; then

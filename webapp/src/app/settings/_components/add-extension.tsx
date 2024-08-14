9a92d570a33410808e188ed545a5ca4f67f00807
Samuel <samuelint@gmail.com>
2024-08-13 20:29:51 -0400

    feat: close modal when adding extension

--- webapp/src/app/settings/_components/add-extension.tsx
+++ webapp/src/app/settings/_components/add-extension.tsx
@@ -1,28 +1,40 @@
 import { useErrorNotification } from '@/app/_components/use-error-notification';
-import { AddLocalExtensionForm } from '@/components/add-local-extension-form';
+import { AddLocalExtensionForm, OnExtensionSubmit } from '@/components/add-local-extension-form';
 import { Button } from '@/components/ui/button';
 
 import { useExtensions } from '@/lib/extension/use-extensions';
 import { Dialog, DialogContent, DialogTrigger } from '@/components/ui/dialog';
+import { useState } from 'react';
 
-export function AddExtension() {
+interface AddExtensionProps {
+  onSubmit?: () => void;
+}
+
+export function AddExtension({ onSubmit }: AddExtensionProps) {
   const { upload, error } = useExtensions();
 
   useErrorNotification(error);
 
+  const onInternalSubmit: OnExtensionSubmit = (file) => {
+    upload(file);
+    onSubmit && onSubmit();
+  };
+
   return (
-    <AddLocalExtensionForm onSubmit={upload} />
+    <AddLocalExtensionForm onSubmit={onInternalSubmit} />
   );
 }
 
 export function AddExtensionDialog() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <Dialog>
+    <Dialog open={isOpen} onOpenChange={setIsOpen}>
       <DialogTrigger>
         <Button className='w-full'>Add Extension</Button>
       </DialogTrigger>
       <DialogContent className="sm:max-w-[425px]">
-        <AddExtension />
+        <AddExtension onSubmit={() => setIsOpen(false)} />
       </DialogContent>
     </Dialog>
   );

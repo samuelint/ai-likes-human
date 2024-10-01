1f290a93acd21cc22744494aa6827c4bbc36efb2
Samuel <samuelint@gmail.com>
2024-10-01 18:24:02 -0400

    fix: system tray string

--- desktop/src/system_tray.rs
+++ desktop/src/system_tray.rs
@@ -1,11 +1,12 @@
-use log::info;
 use tauri::{
     AppHandle, CustomMenuItem, Manager, SystemTrayEvent, SystemTrayMenu, SystemTrayMenuItem,
 };
+static QUIT_APP_LABEL: &'static str = "quit";
+static SHOW_MAIN_WINDOW_APP_LABEL: &'static str = "show";
 
 pub fn build_menu() -> SystemTrayMenu {
-    let menuitem_quit = CustomMenuItem::new("quit".to_string(), "Quit");
-    let menuitem_show = CustomMenuItem::new("show".to_string(), "Show");
+    let menuitem_quit = CustomMenuItem::new(QUIT_APP_LABEL, "Quit AI Likes Human");
+    let menuitem_show = CustomMenuItem::new(SHOW_MAIN_WINDOW_APP_LABEL, "Show Main Window");
     SystemTrayMenu::new()
         .add_item(menuitem_show)
         .add_native_item(SystemTrayMenuItem::Separator)
@@ -14,10 +15,9 @@ pub fn build_menu() -> SystemTrayMenu {
 
 pub fn on_system_tray_event(app: &AppHandle, event: SystemTrayEvent) {
     match event {
-        SystemTrayEvent::MenuItemClick { id, .. } => match id.as_str() {
-            "quit" => app.exit(0),
-            "show" => {
-                info!("Show");
+        SystemTrayEvent::MenuItemClick { id, .. } => match id {
+            id if id == QUIT_APP_LABEL => app.exit(0),
+            id if id == SHOW_MAIN_WINDOW_APP_LABEL => {
                 app.get_window("main").unwrap().show().unwrap();
             }
             _ => {}

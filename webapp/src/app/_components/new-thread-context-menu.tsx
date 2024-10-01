685c3d681087fe413b501bd0e86f95b471e54971
Samuel <samuelint@gmail.com>
2024-10-01 12:49:19 -0400

    feat: remove extensions

--- webapp/src/app/_components/new-thread-context-menu.tsx
+++ webapp/src/app/_components/new-thread-context-menu.tsx
@@ -2,16 +2,11 @@ import {
   ContextMenu,
   ContextMenuContent,
   ContextMenuItem,
-  ContextMenuSeparator,
   ContextMenuTrigger,
 } from '@/components/ui/context-menu';
-import { ErrorDetails } from '@/components/ui/error';
-import { ThreeDotsLoading } from '@/components/ui/loading';
 import { useCreateThread } from '@/lib/assistant/use-create-thread';
-import { useExtensions } from '@/lib/extension/use-extensions';
 
 import { PropsWithChildren } from 'react';
-import { useLocation } from 'wouter';
 
 
 interface Props extends PropsWithChildren { }
@@ -28,40 +23,7 @@ export function NewThreadContextMenu({ children }: Props) {
         <ContextMenuItem inset onSelect={() => createThread()}>
           New
         </ContextMenuItem>
-        <ExtensionSection />
       </ContextMenuContent>
     </ContextMenu>
-  );
-}
-
-function ExtensionSection() {
-  const { data, isLoading, error } = useExtensions();
-  const [_, setLocation] = useLocation();
-  const createThread = useCreateThread({ redirect: true });
-
-  return (
-    <>
-      <ContextMenuSeparator />
-      <ContextMenuItem
-        className='font-bold'
-        inset
-        onSelect={() => setLocation('/settings#extensions')}
-        title='Manage Extensions'
-      >
-        Extensions
-      </ContextMenuItem>
-      { error && <ErrorDetails error={error} /> }
-      { isLoading && <ThreeDotsLoading /> }
-      { data && data.length === 0 && <ContextMenuItem inset disabled>No extensions installed</ContextMenuItem> }
-      { data && data.map((extension) => (
-        <ContextMenuItem
-          key={extension.name}
-          inset
-          onSelect={() => createThread({ assistantId: extension.name })}
-        >
-          { extension.name }
-        </ContextMenuItem>
-      )) }
-    </>
   );
 }

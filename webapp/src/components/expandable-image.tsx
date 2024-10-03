f0b0c2e029259f4fdf1090be009c4e5a047042cb
Samuel <samuelint@gmail.com>
2024-10-03 17:51:28 -0400

    fix: image ui [#77, #74]

--- webapp/src/components/expandable-image.tsx
+++ webapp/src/components/expandable-image.tsx
@@ -2,7 +2,6 @@ import {
   Dialog,
   DialogContent,
   DialogHeader,
-  DialogTitle,
   DialogDescription,
   DialogTrigger,
 } from '@/components/ui/dialog';
@@ -11,23 +10,25 @@ import { ZoomIn } from 'lucide-react';
 import { Button } from './ui/button';
 import { Image, ImageProps } from './image';
 
+interface Props extends ImageProps {
+  imageClassName?: string
+}
 
-export function ExpandableImage({ url, alt, className }: ImageProps) {
+export function ExpandableImage({ url, alt, className, imageClassName }: Props) {
   return (
     <Dialog>
       <DialogTrigger className='relative'>
         <div className='absolute inset-0 flex items-center justify-center group-hover:opacity-100 opacity-0 group-hover:pointer-events-auto transition-opacity duration-200'>
           <Button variant='ghost'><ZoomIn/></Button>
         </div>
-        <Image url={url} alt={alt} className='group-hover:opacity-50 group-hover:pointer-events-none transition-opacity duration-200'/>
+        <Image url={url} alt={alt} className={cn('group-hover:opacity-50 group-hover:pointer-events-none transition-opacity duration-200', imageClassName)}/>
       </DialogTrigger>
       <DialogContent className={cn('max-w-screen h-screen overflow-auto', className)}>
         <DialogHeader>
-          <DialogTitle>{alt}</DialogTitle>
-          <DialogDescription>
-            <Image url={url} alt={alt} className='w-full' />
-          </DialogDescription>
         </DialogHeader>
+        <DialogDescription>
+          <Image url={url} alt={alt} className='w-full' />
+        </DialogDescription>
       </DialogContent>
     </Dialog>
   );

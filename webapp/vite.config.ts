64f46bd1385e604fcb3e8a5c41281e5fb43c1c52
Samuel <samuelint@gmail.com>
2024-10-03 11:27:21 -0400

    feat: setup sentry source map

--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -1,22 +1,33 @@
+import { sentryVitePlugin } from '@sentry/vite-plugin';
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), sentryVitePlugin({
+    org: 'smagny',
+    project: 'ai-likes-human'
+  })],
+
   define: {
     global: {},
   },
+
   test: {
     include: ['**/*.test.ts*'],
     environment: 'jsdom',
     setupFiles: ['./vitest-setup.js'],
     globals: true,
   },
+
   resolve: {
     alias: [
       { find: '@', replacement: path.resolve(__dirname, 'src') },
     ],
   },
+
+  build: {
+    sourcemap: true
+  }
 });

43e07c8ccb092b3379ce2cf62d520077898b4a5c
Samuel <samuelint@gmail.com>
2024-10-03 20:40:02 -0400

    fix: coming back from settings

--- webapp/src/app/settings/page.tsx
+++ webapp/src/app/settings/page.tsx
@@ -1,18 +1,16 @@
 import { useIsInDesktopApp, useScrollToSectionUsingRouteHash } from '@/lib/utils';
 import ApiKeysSection from './_components/api-keys.section';
-import { PageSection } from '@/components/page-section';
 import InternalInfo from './_components/internal-info.section';
 
-
-export default function Settings() {
+export function Settings() {
   const isInDesktopApp = useIsInDesktopApp();
 
   useScrollToSectionUsingRouteHash();
 
   return (
-    <PageSection title="Settings">
+    <div className='w-full flex flex-col gap-4'>
       <ApiKeysSection />
       { process.env.NODE_ENV === 'development' && isInDesktopApp && <InternalInfo /> }
-    </PageSection>
+    </div>
   );
 }
